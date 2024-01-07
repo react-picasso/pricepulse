ce476ada0482e320d73e9a851a757a73165a41bb
Prathamesh Kulkarni <kulkarni@fim.uni-passau.de>
2024-01-07 16:06:05 +0100

    feat: created hero landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+
+import SearchBar from "@/components/SearchBar";
+import HeroCarousel from "@/components/HeroCarousel";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+	return (
+		<>
+			<section className="px-6 md:px-20 py-24">
+				<div className="flex max-xl:flex-col gap-16">
+					<div className="flex flex-col justify-center">
+						<p className="small-text">
+							Smart Shopping Starts Here:
+							<Image
+								src="/assets/icons/arrow-right.svg"
+								alt="right-arrow"
+								width={16}
+								height={16}
+							/>
+						</p>
+						<h1 className="head-text">
+							Unleash the Power of
+							<br />
+							<span className="text-primary">PricePulse</span>
+						</h1>
+						<p className="mt-6">
+							Powerful, self-serving product and growth analytics
+							to help you convert, engage and retain more.
+						</p>
+						<SearchBar />
+					</div>
 
-export default Home
+                    <HeroCarousel />
+				</div>
+			</section>
+
+            <section className="trending-section">
+                <h2 className="section-text">Trending</h2>
+
+                <div className="flex flex-wrap gap-x-8 gap-y-16">
+                    {['Apple iPhone', 'Book', 'Sneakers'].map((product) => (
+                        <div>{product}</div>
+                    ))}
+                </div>
+            </section>
+		</>
+	);
+};
+
+export default Home;
---
+++ components/SearchBar.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import { FormEvent, useState } from "react";
+
+const isValidAmazonURL = (url: string) => {
+	try {
+		const parsedURL = new URL(url);
+		const hostname = parsedURL.hostname;
+
+		if (
+			hostname.includes("amazon.com") ||
+			hostname.includes("amazon.") ||
+			hostname.endsWith("amazon")
+		) {
+			return true;
+		}
+	} catch (error) {
+        console.error(error);
+    }
+
+    return false;
+};
+
+const SearchBar = () => {
+	const [searchPrompt, setSearchPrompt] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+
+	const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+
+		const isValidLink = isValidAmazonURL(searchPrompt);
+
+        if (!isValidLink) {
+            return alert('Please enter a valid Amazon link');
+        }
+
+        try {
+            setIsLoading(true);
+
+            // Scraping logic here
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setIsLoading(false);
+        }
+	};
+
+	return (
+		<form className="flex flex-wrap gap-4 mt-12" onSubmit={handleSubmit}>
+			<input
+				type="text"
+				value={searchPrompt}
+				placeholder="Enter product link"
+				className="searchbar-input"
+				onChange={(e) => setSearchPrompt(e.target.value)}
+			/>
+
+			<button type="submit" className="searchbar-btn" disabled={searchPrompt === ""}>
+				{isLoading ? "Searching..." : "Search"}
+			</button>
+		</form>
+	);
+};
+
+export default SearchBar;
